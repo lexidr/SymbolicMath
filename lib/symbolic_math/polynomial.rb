62afe4e6bcf8bb6c00972d5d5cd7aede273881e5
anastasia0756 <anastasiavolk95@gmail.com>
2026-03-24 22:45:23 +0000

    added a simplification method after differentiation

--- lib/symbolic_math/polynomial.rb
+++ lib/symbolic_math/polynomial.rb
@@ -28,5 +28,35 @@ module SymbolicMath
     def integrate
       Polynomial.new(@terms.map(&:integral))
     end
+  
+    def simplify
+      simplified_terms = @terms.map do |term|
+      simplify_term(term)
+      end
+  
+      simplified_terms.reject! { |t| t.coefficient == 0 }
+  
+      grouped = simplified_terms.group_by { |t| [t.variable, t.exponent] }
+      final_terms = grouped.map do |(var, exp), terms|
+        coeff = terms.sum(&:coefficient)
+        Term.new(coeff, var, exp) if coeff != 0
+      end.compact
+  
+      Polynomial.new(final_terms)
+    end
+
+    private
+
+    def simplify_term(term)
+
+      if term.coefficient == 1 && term.exponent == 1
+        return Term.new(1, term.variable, 1)
+      end
+  
+      if term.exponent == 1
+        return Term.new(term.coefficient, term.variable, 1)
+      end
+      term
+    end
   end
 end
